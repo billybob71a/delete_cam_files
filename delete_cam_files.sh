4d1aa024e72746d3cd10c2f287c2f4ff64d20869
Peter Yung <yungpet@gmail.com>
2022-12-27 16:53:37 -0700

    -modifiying code for accurate logs -modifying code so that it would accuruately log which files are to be deleted and then deleting it after it finds the files -all record of files to be deleted are in one single log file

--- delete_cam_files.sh
+++ delete_cam_files.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 declare -A var_output
-_output=$(find /home/petery/cam1 -mtime +7 -delete > /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
+
+
 check_date() {
-	if [ -z "$_output" ]; then
+	echo "Hi there $1 how are you"
+	if [ -z "$1" ]; then
 		var_date=$(date +%Y-%m-%d)
-		echo "JOB Completed $var_date no files found" >> //home/petery/delete_cam_files-$(date +%Y-%m-%d).log
+		echo "Hello $2 cam number "
+		echo "The $2 JOB has been Completed $var_date no files found" >> //home/petery/delete_cam_files-$(date +%Y-%m-%d).log
 	fi
 }
-#_output=$(find /home/petery/cam1 -mtime +7 -delete > /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
-check_date
-_output=$(find /home/petery/cam2 -mtime +7 -delete >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
-check_date
-_output=$(find /home/petery/cam3 -mtime +7 -delete >> /home/petery/delete_cam_files-$(date +Y-%m-%d).log 2>&1)
-check_date
+
+cameras=("cam1" "cam2" "cam3")
+for cams in ${!cameras[@]}; do
+	echo ${cameras[$cams]}
+	_output[$cams]=" "
+	_output[$cams]+=$(find /home/petery/${cameras[$cams]} -mtime +5 2>&1)
+	_output_arr=($(echo ${_output[$cams]} | awk 'NR>0' RS=' '))
+	for i in ${!_output_arr[@]}; do
+		echo ${_output_arr[i]} >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log
+		rm ${_output_arr[i]}
+	done  
+	# echo ${_output[$cams]} >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 
+	# echo "i will check the date"
+	# echo "the output is $_output[$cams]"
+	# echo "the cam is ${cameras[$cams]} here"
+	check_date ${_output[$cams]} ${cameras[$cams]}
+done
+# _output=$(find /home/petery/cam1 -mtime +7 -delete > /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
+# _output=$(find /home/petery/cam1 -mtime +7 -delete > /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
+#check_date
+# _output=$(find /home/petery/cam2 -mtime +7 -delete >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
+# check_date
+# _output=$(find /home/petery/cam3 -mtime +7 -delete >> /home/petery/delete_cam_files-$(date +Y-%m-%d).log 2>&1)
+# check_date
 #echo "$_output"
 #if [ -z "$_output" ]
 #then
