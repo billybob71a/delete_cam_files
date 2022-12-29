7f6600bd9eb56bfd9234d8c33f35c82ae7e3b1a8
Peter Yung <yungpet@gmail.com>
2022-12-29 16:10:58 -0700

    modifying for loop variables -make the for loop access the values in the arrays directly without need of index variable -adding functionality to delete empty directories

--- delete_cam_files.sh
+++ delete_cam_files.sh
@@ -15,42 +15,25 @@ cameras=("cam1" "cam2" "cam3")
 for cams in ${!cameras[@]}; do
 	echo "Under cam ${cameras[$cams]}" >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log
 	_output[$cams]+=$(find /home/petery/${cameras[$cams]} -mtime +5 )
-	_output_arr=($(echo ${_output[$cams]} | awk 'NR>0' RS=' '))
+	_output_files_arr=($(echo ${_output[$cams]} | awk 'NR>0' RS=' '))
 	len=${#_output_arr[@]}
 	echo "There were ${len} files found which require deletion" >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log
-	for i in ${!_output_arr[@]}; do
-		echo ${_output_arr[$i]} >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log
-		if [[ ${_output_arr[$i]} == *jpg ]] || [[ ${_output_arr[$i]} == *mp4 ]] ; then
-			rm ${_output_arr[$i]}
+	for i in ${_output_files_arr[@]}; do
+		echo "${i}" >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log
+		if [[ ${i} == *jpg ]] || [[ ${i} == *mp4 ]] ; then
+			rm ${i}
 		else
-			rmdir ${_output_arr[$i]};
+			rmdir ${i};
 		fi
-	done  
+	done
+	_output_empty_dir[$cams]=$(find /home/petery/${cameras[$cams]} -type d -empty)
+	_output_empty_dir_arr=($(echo ${_output_empty_dir[$cams]} | awk 'NR>0' RS=' '))
+	len_dir=${#_output_empty_dir_arr[@]}
+	echo "There were ${len_dir} directory found which require deletion" >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log
+	for j in ${_output_empty_dir_arr[@]}; do
+		echo "below ${cameras[$cams]} " >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log
+		echo "the directory that requires deletion is ${j}" >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log
+		rmdir ${j}
+	done
 	check_date ${_output[$cams]} ${cameras[$cams]}
 done
-# _output=$(find /home/petery/cam1 -mtime +7 -delete > /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
-# _output=$(find /home/petery/cam1 -mtime +7 -delete > /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
-#check_date
-# _output=$(find /home/petery/cam2 -mtime +7 -delete >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
-# check_date
-# _output=$(find /home/petery/cam3 -mtime +7 -delete >> /home/petery/delete_cam_files-$(date +Y-%m-%d).log 2>&1)
-# check_date
-#echo "$_output"
-#if [ -z "$_output" ]
-#then
-#	var_date=$(date +%Y-%m-%d)
-#	echo "JOB COMPLETED $date_and_time no files found" > /home/petery/delete1_cam_files-$(date +%Y-%m-%d).log
-#fi
-#
-#_output=$(find /home/petery/cam2 -mtime +7 -delete >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1)
-#
-#f1()
-#{
-#	echo "Hi"
-#	if [ -z "$_output" ]
-#	then
-#		var_date=$(date +%Y-%m-%d)
-#		echo "JOB COMPLETED $var_date no files found" >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log
-#	fi			
-#}
-#find /home/petery/cam3 -mtime +7 -delete >> /home/petery/delete_cam_files-$(date +%Y-%m-%d).log 2>&1
